05e6bfc34a4c5ef906fd5180030bd9e920b096d0
shivaram ganesan <shivaramganesan@shivarams-MacBook-Air.local>
2023-03-17 09:48:23 -0700

    bert index query response time added

--- query.py
+++ query.py
@@ -1,13 +1,13 @@
 import sys
 import bert
 import json
-
+import time
 
 def get_item(indices, resultScores):
-
 	index_lookup = "index_lookup.json"
 	contents = []
 	result = []
+	score_index = 0
 	with open(index_lookup, "r+") as f:
 		contents = json.load(f)
 	if len(contents) > 0:
@@ -15,21 +15,28 @@ def get_item(indices, resultScores):
 			if(i < len(contents)):
 				m = {}
 				m["url"] = contents[i]
-				m["score"] = resultScores[i]
+				m["score"] = resultScores[score_index]
+				score_index+=1
 				result.append(m)
 	return result
+def main():
+	query = "sample query"
+	print(len(sys.argv))
+	if(len(sys.argv)>=2):
+		query = sys.argv[1]	
+	start = time.time()
+	resultIndex, resultScores = bert.query_index(query)
+	if len(resultIndex) == 1 and resultIndex[0] == -1:
+		print("No results found")
+		t = time.time() - start
+		return {"response_time": t/3600, "results":[]}
+	else:
+		results = get_item(resultIndex, resultScores)
+		t = time.time() - start
+		return {"response_time": t/3600, "results":results}
 
 
-query = "sample query"
-print(len(sys.argv))
-if(len(sys.argv)>=2):
-	query = sys.argv[1]	
-resultIndex, resultScores = bert.query_index(query)
-if len(resultIndex) == 1 and resultIndex[0] == -1:
-	print("No results found")
-else:
-	get_item(resultIndex, resultScores)
-
-
+if __name__ == "__main__":
+	print(main())
 
 
